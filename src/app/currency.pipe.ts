38cb3c4d7a1780b3db150c354bacf75ea9380cf6
Noah Van Vaerenbergh <vvnoah@hotmail.com>
2022-10-06 13:49:19 +0200

    closes #12 added currency pipe to joint account balance

--- src/app/currency.pipe.ts
+++ src/app/currency.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CurrencyPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return value + "€";
   }
 
 }
